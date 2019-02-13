588dce919df5cde433a8ec7c95b435dadc1ea264
mr-magnificent <ayush_poddar@live.com>
2019-02-13 03:35:21 -0800

    testing printer pdf

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-import win32print
+import win32print, win32api
 
-printer = win32print.GetDefaultPrinterW()
-print(printer)
+
+name = win32print.GetDefaultPrinterW()
+print(name)
+printDefaults = {"DesiredAccess": win32print.PRINTER_ALL_ACCESS}
+level = 2
+handle = win32print.OpenPrinter(name)
+attributes = win32print.GetPrinter(handle, level)
+attributes['pDevMode'].Duplex = 1
+# win32print.SetPrinter(handle, level, attributes, 0)
+win32print.GetPrinter(handle, level)['pDevMode'].Duplex
+win32api.ShellExecute(0, 'print', 'test.pdf', '.', '/manualstoprint', 0)
+print(name)
